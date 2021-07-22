9c0cca76039200b28de23f0d137da99b26a08b4b
Celeste Dubini <celestedubini@hotmail.com>
2021-07-22 15:38:37 -0300

    Cambie de clase a function el detail

--- client/src/modules/RecipeDetail/index.js
+++ client/src/modules/RecipeDetail/index.js
@@ -1,34 +1,34 @@
 import React from 'react'
-//import { useEffect } from "react";
-import { connect } from 'react-redux';
+import { useEffect } from "react";
+import { connect, useDispatch } from 'react-redux';
 import { getDetail } from '../../store/actions/RecipesActions';
 import "./index.css"
 import { NavLink } from 'react-router-dom';
 
-class RecipeDetail extends React.Component {
+function RecipeDetail(props) {
+const dispatch = useDispatch()
 
+    useEffect(() => {
+        dispatch(getDetail(props.match.params.id))
+    },[])
 
-    componentDidMount() {
-        this.props.getDetail(this.props.match.params.id)
-    }
-
-    render() {
         return (
             <div>
-                {this.props.recipeId.length !== 0 ?
+                {props.recipeId.length !== 0 ?
                     <div className="detail">
                         <div>
                             <h2 className="nombre">
-                                {this.props.recipeId.title}
+                                {props.recipeId.title}
                             </h2>
                         </div>
-                        <img src={this.props.recipeId.image} alt={"img"} className="fotito"/>
+                        <img src={props.recipeId.image} alt={"img"} className="fotito"/>
                         <div className="texto">
-                            <p>Types of Diet: {typeof this.props.recipeId.diets[0] === "object" ? this.props.recipeId.diets.map(diet => Object.values(diet)).join(", ") : this.props.recipeId.diets.map(diet => diet).join(", ")}</p>
-                            <p>Summary: </p><div dangerouslySetInnerHTML={{ __html: this.props.recipeId.summary }}></div>
-                            <p>Score: {this.props.recipeId.spoonacularScore}</p>
-                            <p>Health Score: {this.props.recipeId.healthScore}</p>
-                            <p>Steps: </p><div dangerouslySetInnerHTML={{ __html: this.props.recipeId.instructions? this.props.recipeId.instructions : "Not available"}}></div>
+                            <p>Types of Diet: {typeof props.recipeId.diets[0] === "object" ? 
+                            props.recipeId.diets.map(diet => Object.values(diet)).join(", ") : props.recipeId.diets.map(diet => diet).join(", ")}</p>
+                            <p>Summary: </p><div dangerouslySetInnerHTML={{ __html: props.recipeId.summary }}></div>
+                            <p>Score: {props.recipeId.spoonacularScore}</p>
+                            <p>Health Score: {props.recipeId.healthScore}</p>
+                            <p>Steps: </p><div dangerouslySetInnerHTML={{ __html: props.recipeId.instructions? props.recipeId.instructions : "Not available"}}></div>
                         </div>
                     </div>
                     : <h1 className="Loading">Loading ...</h1>
@@ -39,11 +39,12 @@ class RecipeDetail extends React.Component {
             </div>
         );
     }
-}
+
 
 function mapStateToProps(state) {
     return {
         recipeId: state.recipeDetail
     }
 }
+
 export default connect(mapStateToProps, { getDetail })(RecipeDetail);
